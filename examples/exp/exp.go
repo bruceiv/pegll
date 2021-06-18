e44fa66e7311862ad1cdd89c77a8c482cc31d4fb
Emily Hoppe <hoppe23@up.edu>
2021-06-18 14:52:38 -0700

    Added exp.go

--- examples/exp/exp.go
+++ examples/exp/exp.go
@@ -7,3 +7,35 @@ import (
 	"exp/lexer"
 	"exp/parser"
 )
+
+//Should match
+const ab = `ab`
+const aabc = `aabc`
+//Should fail to match
+const acc = `acc`
+
+func parse(s []rune) bool {
+	// run GLL parser
+	bsrSet, _ := parser.Parse(lexer.New(s))
+	// quit early if parse fails
+	if bsrSet == nil {
+		return false
+	}
+	// check that root covers whole input
+	//root := bsrSet.GetOrderedRoot()
+	//return root.RightExtent() == bsrSet.GetRightExtent();
+}
+
+func parseAndPrint(s string) {
+	if parse([]rune(s)) {
+		fmt.Println("`" + s + "` matched")
+	} else {
+		fmt.Println("`" + s + "` DID NOT match")
+	}
+}
+
+func main() {
+	parseAndPrint(ab)
+	parseAndPrint(aabc)
+	parseAndPrint(acc)
+}
