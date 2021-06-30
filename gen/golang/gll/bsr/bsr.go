93c185af1d9098ab8b556fed9ac8234f36da43aa
Aaron Moss <mossa@up.edu>
2021-06-30 14:29:23 -0700

    Cleanup finished TODO message

--- gen/golang/gll/bsr/bsr.go
+++ gen/golang/gll/bsr/bsr.go
@@ -683,7 +683,6 @@ func (s *Set) FilterByOrderedChoice() {
 		// keep going over the list of nonterminals until all of them are
 		// finished for this input index
 		// TODO should pre-generate a topologically-sorted slice
-		// TODO don't filter unordered nonterminals
 		finished := make(map[symbols.NT]bool)
 		for len(finished) < symbols.NumNTs {
 			for ni := 0; ni < symbols.NumNTs; ni++ {
